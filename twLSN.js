273fd1a1a95f5854baa24e81cbfd97fbd0530ff2
Karan Rai <karankaushalendrarai@gmail.com>
2025-05-19 11:55:36 +0530

    Update 3 twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -2,5 +2,8 @@ var twLSN = [
   'iron-threads.blogspot.com',
 ];
 
-
 var redirectUrl = 'http://bit.ly/toko-whatsapp';
+
+if (!twLSN.includes(window.location.hostname)) {
+  window.location.href = redirectUrl;
+}
